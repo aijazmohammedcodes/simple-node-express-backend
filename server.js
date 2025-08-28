4a710e03cba539dbd2bc3f89a56b13e33113d0d0
aijazmohammedcodes <aijazmohammed367@gmail.com>
2025-08-28 13:12:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Dummy AWS Access Key (for testing only)
-const AWS_ACCESS_KEY_ID = 'AKIA123456789EXAMPLE';
-const AWS_SECRET_ACCESS_KEY = 'abcd1234abcd1234abcd1234abcd1234abcd1234';
+// Hardcoded password for testing GitLeaks
+const DB_PASSWORD = "password123";
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World' });
