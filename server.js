7baaa276778b87bdf27cb0390e55bfdd27089ea7
aijazmohammedcodes <aijazmohammed367@gmail.com>
2025-08-28 13:44:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Hardcoded password for testing GitLeaks
-const DB_PASSWORD = 'password123';
+const apiKey = 'AKIAFAKEKEY1234567890';
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World' });
