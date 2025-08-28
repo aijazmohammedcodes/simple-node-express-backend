9f1f12dfd577ef4a1839c2ce46edb0f7f79b8569
aijazmohammedcodes <aijazmohammed367@gmail.com>
2025-08-28 12:54:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Dummy AWS Access Key (for testing only)
+const AWS_ACCESS_KEY_ID = "AKIA123456789EXAMPLE";
+const AWS_SECRET_ACCESS_KEY = "abcd1234abcd1234abcd1234abcd1234abcd1234";
+
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World' });
 });
